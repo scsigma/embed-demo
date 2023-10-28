790a9b7b5c7e4f1b71da9ec4202b1fc7c96d052e
scsigma <sully@sigmacomputing.com>
2023-10-28 16:48:42 -0700

    checkpoint before editor mode

--- src/components/analyticsPage/AnalyticsPage.js
+++ src/components/analyticsPage/AnalyticsPage.js
@@ -8,56 +8,44 @@ import DrillDownPNG from './images/drill_down.png';
 import BarChartComputerDrillDown from './images/drill_down_comuter_analytics_page.png';
 import SaveBookmarkPNG from './images/save_bookmark.png';
 
+// Correct Import
+import FullPageEmbedPNG from './images/full_page_embed.png';
+import FullPageEmbedWithDrilldownPNG from './images/full_page_embed_drilldown.png';
+import FullPageEmbedWithComputerBarchartPNG from './images/full_page_embed_computer_barchart.png';
+import FullPageEmbedSaveBookmarkPNG from './images/full_page_embed_save_bookmark.png';
+import FullPageEmbedEditorModePNG from './images/full_page_embed_editor_mode.png';
+
 
 export const AnalyticsPage = ({ currStep }) => {
 
-  const renderInOpaque = (currStep) => {
-    if (currStep > 0 && currStep < 2) {
+  const screenshotRender = (currStep) => {
+    if (currStep === 0) {
         return (
-            <div className="opaque-div" style={{width: "1222px", height: "652px", backgroundColor: "rgb(217, 217, 217, 0.65)", zIndex:"1", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <img src={DrillDownPNG} style={{width:"500px"}}/>
-            </div>
-        );
-    } else if (currStep > 2 && currStep < 4) {
+            <img src={FullPageEmbedPNG} style={{width: "100%"}}/>
+        )
+    } else if (currStep > 0 & currStep < 2) {
         return (
-            <div className="opaque-div" style={{width: "1222px", height: "652px", backgroundColor: "rgb(217, 217, 217, 0.65)", zIndex:"1", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <img src={SaveBookmarkPNG} style={{width:"600px"}}/>
-            </div>
-        );
-    } else {
+            <img src={FullPageEmbedWithDrilldownPNG} style={{width: "100%"}} />
+        )
+    } else if (currStep >= 2 & currStep < 3 || currStep >= 4 & currStep < 5) {
         return (
-            <></>
-        );
+            <img src={FullPageEmbedWithComputerBarchartPNG} style={{width: "100%"}} />
+        )
+    } else if (currStep >= 3 & currStep < 4){
+        return (
+            <img src={FullPageEmbedSaveBookmarkPNG} style={{width: "100%"}} />
+        )
+    } else if (currStep >= 5) {
+        return (
+            <img src={FullPageEmbedEditorModePNG} style={{width: "100%"}} />
+        )
     }
   }
 
 
   return (
-   <div className="analytics-page-container" style={{"width":"1222px", "height":"723px", borderRadius: "18px", backgroundColor: "#ffffff", display: "flex", flexDirection:"column", position:"relative", alignItems:"center"}}>
-    
-    {/* THIS IS WHERE WE WILL RENDER THE OPAQUE PARTS WITH THEIR CORRESPONDING VALUES WHEN WE NEED TO */}
-    {renderInOpaque(currStep)}
-    
-    <div className="title-filters-kpis-container" style={{"width":"1176px", height:"201px", position:"absolute", top:"20px"}}>
-        <TitleFilterKPIs />
+    <div className="analytics-page-container" style={{"width":"1222px", "height":"723px", borderRadius: "18px", backgroundColor: "#ffffff", display: "flex", flexDirection:"column", position:"relative", alignItems:"center"}}>
+        {screenshotRender(currStep)}
     </div>
-
-    <div className="charts-container" style={{"width":"1183px", height:"393px", position:"absolute", top:"242px", display:"flex", justifyContent:"space-between"}}>
-        <div className="barchart-container" style={{width:"573px", height: "393px"}}>
-            {currStep <= 1 ? 
-              <BarChart /> 
-              : <img src={BarChartComputerDrillDown} style={{width: "100%"}}/>
-            }
-            {/* <BarChart /> */}
-        </div>
-        <div className="pivot-container" style={{width:"573px", height: "389px"}}>
-            <img src={PivotTable} style={{width: "100%"}}/>
-        </div>
-    </div>
-
-    <div className="navbar-container" style={{"width":"100%", height:"70px", position:"absolute", bottom:"0px"}}>
-        <SigmaFooter />
-    </div>
-   </div>
   );
 };
