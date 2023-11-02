6056612a13acc09ea39906f6db2c337ce498cf81
Oscar Bashaw <oscar.bashaw@MacBook-Pro.lan>
2023-11-02 13:31:52 -0700

    moved header section into its own component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import PlugsLogo from './components/graphics/plugs_electronics_logo_green.png';
 import { AnalyticsPage } from './components/analyticsPage/AnalyticsPage.js';
 import { SummaryPage } from './components/summaryPage/SummaryPage.js';
 import { ProgressBar } from './components/progressBar/ProgressBar.js';
-import SigmaLogo from './components/graphics/sigma-full-black.png'
+import { Header } from './components/nativeAppComponents/header.js';
+
 
 const App = () => {
 
@@ -39,47 +40,7 @@ const App = () => {
 
   return (
     <div className='page-container' style={{"display":"flex", flexDirection: "column", "alignItems":"center", "minHeight":"100vh", "minWidth":"100vw", "position":"fixed"}}>
-      <div className='header-container' style={{height: "100px", width: "100%", display: "flex", marginBottom: "5px"}}>
-        <div className='sigma-logo-container' style={{width: "300px", height: "100%", display: "flex", alignItems: "center", justifyContent:"center"}}>
-            <a href="https://www.sigmacomputing.com/product/embedded-analytics" style={{display: "flex", alignItems: "center"}}><img src={SigmaLogo} style={{height: "60px"}}></img></a>
-        </div>
-        <div className='button-container' style={{position: "relative", width: "80%", right: "0px", display: "flex", flexDirection: "row-reverse"}}>
-          <div className='free-trial-container' style={{width: "300px", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <form action="https://www.sigmacomputing.com/free-trial-embedded?trial_theme=Embedded%20Analytics">
-              <button style={{
-                  padding: "14px",
-                  backgroundColor: "#0004E1",
-                  color: "#ffffff",
-                  border: "none",
-                  borderRadius: "5px",
-                  fontFamily: "Lato",
-                  cursor: "pointer",
-                  fontSize: "16px",
-                  fontWeight: "700"
-                }}>
-                  FREE TRIAL
-                </button>
-            </form>
-          </div>
-          <div className='request-demo-container' style={{width: "300px", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-              <form action="https://www.sigmacomputing.com/request-a-demo">
-                <button style={{
-                  padding: "14px",
-                  backgroundColor: "#ffffff",
-                  color: "#171717",
-                  border: "2px solid #171717",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  fontFamily: "Lato",
-                  fontSize: "16px",
-                  fontWeight: "700"
-                }}>
-                  REQUEST A DEMO
-                </button>
-              </form>
-          </div>
-        </div>
-      </div>
+      <Header/>
       <div className='app-container' style={{"width":"1468px","height":"844px", "display":"flex","flexDirection":"column"}}>
         {/* INTRO / PILLARS SCREEN */}
         { currPage === 0 && (
