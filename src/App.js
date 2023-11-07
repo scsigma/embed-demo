4d27a8b73f43f3b16b97dac4c63cb3890d91d874
scsigma <sully@sigmacomputing.com>
2023-11-07 08:28:27 -0800

    mac border done

--- src/App.js
+++ src/App.js
@@ -9,45 +9,7 @@ import { SummaryPage } from './components/summaryPage/SummaryPage.js';
 import { ProgressBar } from './components/progressBar/ProgressBar.js';
 import { Header } from './components/nativeAppComponents/header.js';
 import { OutroPage } from './components/outroPage/outroPage.js';
-
-import styled from 'styled-components';
-
-const MacWindow = styled.div`
-  border: 1px solid #b0b0b0;
-  border-radius: 7px;
-  background-color: white;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-`;
-
-const TitleBar = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  height: 30px;
-  background-color: #b0b0b0;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  padding-left: 10px;
-`;
-
-const Button = styled.div`
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  margin: 0 3px;
-`;
-
-const RedButton = styled(Button)`
-  background-color: #ff5f57;
-`;
-
-const YellowButton = styled(Button)`
-  background-color: #ffdb58;
-`;
-
-const GreenButton = styled(Button)`
-  background-color: #28c940;
-`;
+import { MacWindow, TitleBar, RedButton, YellowButton, GreenButton } from './components/macBorder/MacBorder.js';
 
 
 const App = () => {
--- src/components/macBorder/MacBorder.js
+++ src/components/macBorder/MacBorder.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const MacWindow = styled.div`
-  border: 1px solid #b0b0b0;
-  border-radius: 5px;
+export const MacWindow = styled.div`
+  border: 1px solid #dedee3;
+  border-radius: 7px;
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 `;
 
-const TitleBar = styled.div`
+export const TitleBar = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: center;
   height: 30px;
-  background-color: #b0b0b0;
+  background-color: #dedee3;
   border-top-left-radius: 5px;
   border-top-right-radius: 5px;
+  padding-left: 10px;
 `;
 
 const Button = styled.div`
@@ -23,34 +24,29 @@ const Button = styled.div`
   height: 12px;
   border-radius: 50%;
   margin: 0 3px;
-  cursor: pointer;
 `;
 
-const RedButton = styled(Button)`
+export const RedButton = styled(Button)`
   background-color: #ff5f57;
 `;
 
-const YellowButton = styled(Button)`
+export const YellowButton = styled(Button)`
   background-color: #ffdb58;
 `;
 
-const GreenButton = styled(Button)`
+export const GreenButton = styled(Button)`
   background-color: #28c940;
 `;
 
-const Content = styled.div`
-  padding: 20px;
-`;
-
-export const MacBorder = ({ children }) => {
-  return (
-    <MacWindow>
-      <TitleBar>
-        <RedButton />
-        <YellowButton />
-        <GreenButton />
-      </TitleBar>
-      <Content>{children}</Content>
-    </MacWindow>
-  );
-};
+// export const MacBorder = ({ children }) => {
+//   return (
+//     <MacWindow>
+//       <TitleBar>
+//         <RedButton />
+//         <YellowButton />
+//         <GreenButton />
+//       </TitleBar>
+//       <Content>{children}</Content>
+//     </MacWindow>
+//   );
+// };
